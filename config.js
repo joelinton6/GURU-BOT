249491fa5b924cdc6df91de3b50d94ddcadc4a45
joelinton6 <154518343+joelinton6@users.noreply.github.com>
2024-01-05 21:41:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['254705121317','cyprian' , true],
-  ['254705121317', 'cyprian', true], 
+  ['254113100541','Antidote💢' , true],
+  ['254113100541', 'Antidote💢', true], 
   [''] 
 ] //Number of owners
 
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'Cyprian'
+global.botname = 'Antidote'
 global.premium = 'false'
-global.packname = 'Cyprian' 
-global.author = '@Cypriqn' 
+global.packname = 'Antifote' 
+global.author = '@Antidote' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
